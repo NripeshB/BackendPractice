8f0531cfdce80e54e59ac23d81074f8210f269c8
NripeshB <nripesh.bhusal@gmail.com>
2025-09-02 22:07:10 +0530

    testing the props naming

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,16 +1,27 @@
+const Hello = (goob) => {
+  console.log("This is goob")
+  console.log(goob)
+  return (
+    <div>
+      <p>
 
-  const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+        Hello {goob.name}, you are {goob.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
