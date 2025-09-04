b8ef7a81723b5989e951e672ad2ef9bcdbbaf28b
NripeshB <nripesh.bhusal@gmail.com>
2025-09-04 20:56:30 +0530

    how to debug efficiently

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,38 +1,53 @@
 import { useState } from 'react'
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
 
-//  const handleLeftClick = () => {
-//   const newClicks = { 
-//     ...clicks, 
-//     left: clicks.left + 1 
-//   }
-//   setClicks(newClicks)
-// }
-
-// const handleRightClick = () => {
-//   const newClicks = { 
-//     ...clicks, 
-//     right: clicks.right + 1 
-//   }
-//   setClicks(newClicks)
-// }
-
-const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 })
-
-const handleRightClick = () =>
-  setClicks({ ...clicks, right: clicks.right + 1 })
-
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = (props) => { 
+  console.log("this is the props: " , props)
+  const { onClick, text } = props
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
